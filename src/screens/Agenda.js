f0483406b10942fb5cb09a53fef64ff59192ed85
SMARAPD - Felipe Oliveira <FAOLIVEIRA@SMARAPD.COM.BR>
2019-06-25 14:01:23 -0300

    Continuação projeto de tasks.

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -4,20 +4,37 @@ import moment from 'moment'
 import 'moment/locale/pt-br'
 import todayImage from '../../assets/imgs/today.jpg'
 import commonStyles from '../commonStyles'
+import * as Font from 'expo-font'
 
 export default class Agenda extends Component {
+    constructor() {
+        super()
+        this.state = {
+            fontLoaded: false
+        }
+    }
+    
+    async componentDidMount() {
+        await Font.loadAsync({
+            'Lato': require('../../assets/fonts/Lato.ttf')
+        })
+        this.setState({ fontLoaded: true })
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <ImageBackground
                     source={todayImage}
                     style={styles.background}>
-                    <View style={styles.titleBar}>
-                        <Text style={styles.title}>Hoje</Text>
-                        <Text style={styles.subtitle}>
-                            {moment().locale('pt-br').format('ddd, D [de] MMMM')}
-                        </Text>
-                    </View>
+                    {this.state.fontLoaded ? (
+                        <View style={styles.titleBar}>
+                            <Text style={styles.title}>Hoje</Text>
+                            <Text style={styles.subtitle}>
+                                {moment().locale('pt-br').format('ddd, D [de] MMMM')}
+                            </Text>
+                        </View>
+                    ) : null}
                 </ImageBackground>
                 <View style={styles.taskContainer}>
                     <Text>Tarefa 1</Text>
@@ -41,12 +58,14 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end'
     },
     title: {
+        fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.secondary,
         fontSize: 50,
         marginLeft: 20,
         marginBottom: 10
     },
     subtitle: {
+        fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.secondary,
         fontSize: 20,
         marginLeft: 20,
